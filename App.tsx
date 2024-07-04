34f8c140fd2e97b5e1bb8021c9da3811404d9197
João Souza <joaosvieira04@gmail.com>
2024-07-04 19:55:38 -0300

    feat: Cria a primeira página da aplicação

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, Pressable, View } from 'react-native';
+import Header from './components/Header'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header/>
+      <Text style={styles.text}>Bem vindo a</Text>
+      <Text style={styles.title}>My Weather</Text>
+      <Text style={styles.text}>Aqui você poderá ver o clima presente na sua ou em mais cidades!</Text>
+      <Image 
+        style={styles.image}
+        source={require('./assets/images/standing.png')}
+        resizeMode='contain'
+      />
+      <Pressable
+        style={({pressed}) => [
+          {
+            backgroundColor: pressed ? '#027efa' : '#52a6fa'
+          },
+          styles.button]}
+        >
+        <Text style={styles.buttonText} >Vamos lá!</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +29,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00004d',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20
   },
+  title: {
+    fontWeight: '700',
+    color: 'white',
+    fontSize: 40,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  image: {
+    height: 250,
+    width: 250,
+    marginBottom: 20
+  },
+  button: {
+    width: '50%',
+    borderWidth: 2,
+    borderColor: '#00004d',
+    borderRadius: 25,
+    padding: 12,
+    margin: 30,
+  },
+  changedButton: {
+    backgroundColor: '#52a6fa',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  }
 });
